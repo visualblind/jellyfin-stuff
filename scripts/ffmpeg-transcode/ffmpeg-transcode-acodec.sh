25ad09962d72a3c3b70eb4913c1be6874ae28c03
Travis Runyard <travisrunyard@gmail.com>
2020-04-13 01:45:25 -0700

    updated ffmpeg script

--- scripts/ffmpeg-transcode/ffmpeg-transcode-acodec.sh
+++ scripts/ffmpeg-transcode/ffmpeg-transcode-acodec.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-TEMPDIR="/temp/ffmpeg"
+TEMPDIR="/mnt/pool0/p0ds0smb/temp/ffmpeg"
 WORKDIR="$TEMPDIR/working"
 declare -a LOG
 shopt -s globstar
@@ -40,36 +40,36 @@ done
 cd $TEMPDIR
 echo -e "DEBUG:\n\$TEMPDIR = $TEMPDIR\n\$WORKDIR = $WORKDIR"
 [ -d "$WORKDIR" ] || mkdir "$WORKDIR" || exit $?
-FILENAME=$(find . ! -path "*$(basename "$WORKDIR")/*" -regextype posix-extended -regex '.*.(mkv|mp4)' -print)
+FILENAME=$(find . ! -path "*$(basename "$WORKDIR")/*" -regextype posix-extended -regex '.*.(mkv|mp4)$' -print)
 if [ -n "$FILENAME" ]; then
   for f in $FILENAME; do
-  	LOG+=("$f")
-	AUDIOFORMAT=($(ffprobe -loglevel error -select_streams a:0 -show_entries stream=codec_name,channels -of default=nw=1:nk=1 "$f"))
+    LOG+=("$f")
+    AUDIOFORMAT=($(ffprobe -loglevel error -select_streams a:0 -show_entries stream=codec_name,channels -of default=nw=1:nk=1 "$f"))
   	if ! [ "${AUDIOFORMAT[0]}" = "aac" ]; then
-
-  	# Build ffmpeg array within the loop to populate variables
-    args=(
-		-i "${f}"
-		-map 0:v:0
-		-map 0:a:0
-		-map 0:s?
-		-c:v copy
-		-ac "${AUDIOFORMAT[1]}"
-		-ar 48000
-		-metadata:s:a:0
-		language=eng
-		-c:a aac
-		-c:s copy
-		"$WORKDIR/$(basename "${f}")"
-		)
-
-	  echo -e '\nffprobe detected '${AUDIOFORMAT[0]}' in the default audio stream with '${AUDIOFORMAT[1]}' channels\nPreparing to convert audio codec to AAC...\n' ; sleep 1
-    echo -e "\nDEBUG: ffmpeg ${args[@]}\n"
-    ffmpeg "${args[@]}" || break
-    echo -e '\nMoving '$WORKDIR/$(basename "${f}")' back to source directory name '$(dirname "${f}")'\n'; sleep 1
-    mv -ufv "$WORKDIR/$(basename "${f}")" "$(dirname "${f}")" || break
-    fi
+      # Build ffmpeg array within the loop to populate variables
+      args=(
+      -i "${f}"
+      -map 0:v:0
+      -map 0:a:0
+      -map 0:s?
+      -c:v copy
+      -ac "${AUDIOFORMAT[1]}"
+      -ar 48000
+      -metadata:s:a:0
+      language=eng
+      -c:a aac
+      -c:s copy
+      "$WORKDIR/$(basename "${f}")"
+      )
+# args=("-i ${f}" "${args[@]}")
+#	args+=("$WORKDIR/$(basename "${f}")")
+	echo -e '\nffprobe detected '${AUDIOFORMAT[0]}' in the default audio stream with '${AUDIOFORMAT[1]}' channels\nPreparing to convert audio codec to AAC...\n' ; sleep 1
+	echo -e "\nDEBUG: ffmpeg ${args[@]}\n"
+	ffmpeg "${args[@]}" || break
+	echo -e '\nMoving '$WORKDIR/$(basename "${f}")' back to source directory name '$(dirname "${f}")'\n'; sleep 1
+	mv -ufv "$WORKDIR/$(basename "${f}")" "$(dirname "${f}")" || break
+	fi
   done
-printf '\nPROCESSED:%s\n'
-for i in "${LOG[@]}"; do echo "$i"; done
-fi
+  printf '\nPROCESSED:%s\n'
+  for i in "${LOG[@]}"; do echo "$i"; done
+ fi
